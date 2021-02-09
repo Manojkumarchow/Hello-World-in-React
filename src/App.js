294a5c19fe719f1c7c16532f86ec017fea83ed5f
Manoj Chowdary <manojchow72@gmail.com>
2021-02-09 08:26:55 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import HelloComponent from "./components/HelloComponent";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloComponent name="Manoj Chowdary" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
